559c3b1b68009d69ffd4e7f3e6d10ec00b71ba28
Michael Harris <mike.ghazi.harris@gmail.com>
2019-08-16 18:42:12 -0500

    first commit, basic mvc file structure, hello world

---
+++ src/main/java/com/github/cosmicdisorder/antefare/AntefareApplication.java
@@ -0,0 +1,12 @@
+package com.github.cosmicdisorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntefareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AntefareApplication.class, args);
+    }
+}
