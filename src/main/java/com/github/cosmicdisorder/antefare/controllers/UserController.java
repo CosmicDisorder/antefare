384fd645cec357de359b6959e737753e81b85a9e
Michael Harris <mike.ghazi.harris@gmail.com>
2019-08-29 16:02:15 -0500

    user not acting as intended, but it's compiling ;)

--- src/main/java/com/github/cosmicdisorder/antefare/controllers/UserController.java
+++ src/main/java/com/github/cosmicdisorder/antefare/controllers/UserController.java
@@ -1,12 +1,27 @@
 package com.github.cosmicdisorder;
 
+import com.github.cosmicdisorder.antefare.models.data.UserDao;
+import com.github.cosmicdisorder.antefare.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
+
+    @Autowired
+    private UserDao userDao;
+
 
     @RequestMapping("")
     public String index(Model model) {
@@ -20,4 +35,25 @@ public class UserController {
         return "user/login";
     }
 
+    @RequestMapping(value = "login", method = RequestMethod.POST)
+    public String userLogin(Model model, @ModelAttribute("inputEmail") String inputEmail,
+            @ModelAttribute("inputPassword") String inputPassword, Errors errors) {
+        if (errors.hasErrors()) {
+            return "user/login";
+        }
+        ArrayList<User> Users = new ArrayList<User>();
+        for(User user : userDao.findAll()) {
+            Users.add(user);
+            if(inputEmail == user.getEmail()) {
+                User activeUser = user;
+                if (inputPassword == activeUser.getPassword()) {
+                    model.addAttribute("activeUser", activeUser.getName());
+                    return "antefare/index";
+                }
+            }
+        }
+
+
+        return "user/login";
+    }
 }
--- src/main/java/com/github/cosmicdisorder/antefare/models/data/UserDao.java
+++ src/main/java/com/github/cosmicdisorder/antefare/models/data/UserDao.java
@@ -4,6 +4,7 @@ import com.github.cosmicdisorder.antefare.models.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Repository
 @Transactional
